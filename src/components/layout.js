e17ee4ff4f5d54c7eee00d6e4a4529d9f9422103
Scott Branch <scottebranch@gmail.com>
2021-01-13 18:41:41 -0800

    adding redirect

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@
  * See: https://www.gatsbyjs.com/docs/use-static-query/
  */
 
-import React from "react"
+import React, { useEffect } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
@@ -13,6 +13,11 @@ import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => {
+
+  const redirect = () => {
+    window !== undefined && window.location.replace('https://airborneecs.com/')
+  }
+
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
       site {
@@ -25,25 +30,8 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+      {/*<Header siteTitle={data.site.siteMetadata?.title || `Title`} />*/}
+      {redirect()}
     </>
   )
 }
