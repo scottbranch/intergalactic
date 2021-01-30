393b8cbfaded106fd61d135e036b9e45206e8526
Scott Branch <scottebranch@gmail.com>
2021-01-29 17:39:46 -0800

    getting locomotive scroll setup

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,14 +9,12 @@ import React, { useEffect } from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
+import Scroll from "./locomotiveScroll"
 import Header from "./header"
 import "./layout.css"
+import "./locomotive-scroll.css"
 
-const Layout = ({ children }) => {
-
-  useEffect(() => {
-    window.location.replace('https://airborneecs.com/')
-  },[])
+const Layout = ({ children, location }) => {
 
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -30,7 +28,26 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      {/*<Header siteTitle={data.site.siteMetadata?.title || `Title`} />*/}
+      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+
+      {/* Here we pass the callbacks to the component. Anything that impacts the innerHeight, for example: Font Loaded */}
+      <Scroll callbacks={location} />
+
+      <div
+        id="container"
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `120px 1.45rem 1.0875rem 1.45rem`,
+        }}
+      >
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </div>
     </>
   )
 }
