c4af52f03c68fb32a25e066d2f5e54fef30e0a8b
scottbranch <scottebranch@gmail.com>
2021-03-14 13:15:20 -0700

    structuring box checked section

--- src/components/homepage/BoxesChecked.js
+++ src/components/homepage/BoxesChecked.js
@@ -2,12 +2,40 @@ import React from "react"
 import styled from "styled-components"
 
 const BoxesChecked = () => {
-  return <StyledSection data-scroll-section></StyledSection>
+  return (
+    <StyledSection data-scroll-section>
+      <StyledHeading>Boxes Checked</StyledHeading>
+      <StyledUl>
+        <StyledLi>Sole-sourced on major platforms.</StyledLi>
+        <StyledLi>Sole-sourced on major platforms.</StyledLi>
+        <StyledLi>Sole-sourced on major platforms.</StyledLi>
+        <StyledLi>Sole-sourced on major platforms.</StyledLi>
+        <StyledLi>Sole-sourced on major platforms.</StyledLi>
+      </StyledUl>
+    </StyledSection>
+  )
 }
 
 const StyledSection = styled.section`
   padding: 180px 60px;
   background: ${({ theme }) => theme.colors.cream};
 `
+
+const StyledUl = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 200px;
+  max-width: 1250px;
+`
+
+const StyledLi = styled.li`
+  font-size: 25px;
+  list-style: none;
+  padding: 40px 0 40px 20px;
+  border-top: 1px solid ${({ theme }) => theme.colors.black};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.black};
+`
+
+const StyledHeading = styled.h3``
 
 export default BoxesChecked
