b1240407837d3223672cea60b7b7575d590f77b2
scottbranch <scottebranch@gmail.com>
2021-04-21 17:13:53 -0700

    blog stuff

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,20 +1,40 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.com/docs/node-apis/
- */
+const path = require("path")
 
- exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
-   if (stage === "build-html" || stage === "develop-html") {
-     actions.setWebpackConfig({
-       module: {
-         rules: [
-           {
-             test: /locomotive-scroll/,
-             use: loaders.null(),
-           },
-         ],
-       },
-     })
-   }
- }
+exports.onCreateWebpackConfig = ({ stage, loaders, actions }) => {
+  if (stage === "build-html" || stage === "develop-html") {
+    actions.setWebpackConfig({
+      module: {
+        rules: [
+          {
+            test: /locomotive-scroll/,
+            use: loaders.null(),
+          },
+        ],
+      },
+    })
+  }
+}
+
+exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
+  const pages = await graphql(`
+    {
+      allPrismicBlogPost {
+        nodes {
+          uid
+        }
+      }
+    }
+  `)
+
+  pages.data.allPrismicBlogPost.nodes.forEach(edge => {
+    createPage({
+      path: `/commslink/${edge.uid}`,
+      component: path.resolve("src/templates/blogPost.js"),
+      context: {
+        uid: edge.uid,
+      },
+    })
+  })
+}
--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -6,44 +6,46 @@ import styled from "styled-components"
 import { Helmet } from "react-helmet"
 
 const Post = ({ data }) => {
-  const { data } = prismicJournal
+  //const { data } = prismicJournal
 
   return (
     <Layout>
       <SEO title="Blog" />
       <Helmet>
-        <body class="blog"/>
+        <body class="blog" />
       </Helmet>
       <p>testing</p>
     </Layout>
   )
+}
+
 export default Post
 
-export const pageQuery = graphql`
-  query JournalPost($uid: String!) {
-    prismicJournal(uid: { eq: $uid }) {
-      uid
-      data {
-        title {
-          text
-        }
-        description {
-          text
-        }
-        post_id
-      }
-    }
-    allPrismicJournal(sort: {order: DESC, fields: id}) {
-      nodes {
-        uid
-        slugs
-        data {
-          post_id
-          title {
-            text
-          }
-        }
-      }
-    }
-  }
-`
+// export const pageQuery = graphql`
+//   query JournalPost($uid: String!) {
+//     prismicJournal(uid: { eq: $uid }) {
+//       uid
+//       data {
+//         title {
+//           text
+//         }
+//         description {
+//           text
+//         }
+//         post_id
+//       }
+//     }
+//     allPrismicJournal(sort: {order: DESC, fields: id}) {
+//       nodes {
+//         uid
+//         slugs
+//         data {
+//           post_id
+//           title {
+//             text
+//           }
+//         }
+//       }
+//     }
+//   }
+// `
