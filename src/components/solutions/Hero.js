e7a936656e721188131aa98e0b0160c272bc6b82
Eric Grucza <egrucza@credera.com>
2021-04-25 15:42:09 -0500

    updating images

--- src/components/solutions/Hero.js
+++ src/components/solutions/Hero.js
@@ -33,7 +33,7 @@ const StyledSection = styled.section`
   background-position: bottom center;
 
   @media screen and (max-width: 768px) {
-    height: 100vh;
+    height: 700px;
   }
 `
 
@@ -47,7 +47,9 @@ const HeadingContainer = styled.div`
 
   @media screen and (max-width: 768px) {
     flex-direction: column;
-    justify-content: space-evenly;
+    justify-content: initial;
+    padding: 0 25px;
+    align-items: initial;
   }
 `
 
@@ -59,7 +61,8 @@ const StyledHeading = styled.h2`
 
   @media screen and (max-width: 768px) {
     flex: initial;
-    margin-top: 200px;
+    margin-top: auto;
+    margin-bottom: auto;
     font-size: 52px;
     line-height: 100%;
   }
