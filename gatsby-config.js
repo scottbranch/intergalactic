01920326abb5f8309a71321730337230ab66e476
Scott Branch <scott.branch@credera.com>
2021-12-09 17:51:48 -0800

    fixing build issue

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,27 +29,6 @@ module.exports = {
       },
     },
     {
-       resolve: `gatsby-plugin-google-gtag`,
-       options: {
-         // You can add multiple tracking ids and a pageview event will be fired for all of them.
-         trackingIds: [
-           "UA-196848983-2"
-         ],
-         // This object gets passed directly to the gtag config command
-         // This config will be shared across all trackingIds
-         gtagConfig: {
-           optimize_id: "OPT_CONTAINER_ID",
-           anonymize_ip: true,
-           cookie_expires: 0,
-         },
-         // This object is used for configuration specific to this plugin
-         pluginConfig: {
-           // Puts tracking script in the head instead of the body
-           head: true
-         },
-       },
-     },
-    {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
